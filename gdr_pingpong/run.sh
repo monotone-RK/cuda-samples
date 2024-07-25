b5c4ab369d148ca5d0d5b75bfe2d20b9321ceab3
monotone-RK <kobaya4121@gmail.com>
2024-07-26 04:45:57 +0900

    通信性能のログをCSVファイルに書き出すロジックを追加．通信性能可視化用Pythonスクリプトを追加

--- gdr_pingpong/run.sh
+++ gdr_pingpong/run.sh
@@ -2,7 +2,7 @@
 #------- qsub option -----------
 #PBS -A CCUSC
 #PBS -q gpu
-#PBS -l elapstim_req=00:10:00
+#PBS -l elapstim_req=00:15:00
 #PBS -T openmpi
 #PBS -b 2
 #PBS -v NQSV_MPI_VER=4.1.6/nvhpc23.1-cuda12.0
@@ -10,4 +10,6 @@
 #------- Program execution ----------
 cd $PBS_O_WORKDIR
 module load openmpi/"$NQSV_MPI_VER"
-mpirun ${NQSV_MPIOPTS} -report-bindings -bind-to none -np 2 -npernode 1 ./gdr_pingpong -max_elements 268435456 -num_iterations 1000
+mpirun ${NQSV_MPIOPTS} -report-bindings -bind-to none -np 2 -npernode 1 ./gdr_pingpong -max_elements 268435456 -num_iterations 1000 -output_file gdr.csv
+mpirun ${NQSV_MPIOPTS} -report-bindings -bind-to none -np 2 -npernode 1 -x UCX_TLS=sm,rc,ud,dc,ud_x,tcp,rdmacm,sockcm,self,cuda_ipc ./gdr_pingpong -max_elements 268435456 -num_iterations 100 -output_file no_gdr_1.csv
+mpirun ${NQSV_MPIOPTS} -report-bindings -bind-to none -np 2 -npernode 1 -x UCX_TLS=sm,rc,ud,dc,ud_x,tcp,rdmacm,sockcm,self,cuda_ipc,cuda_copy ./gdr_pingpong -max_elements 268435456 -num_iterations 1000 -output_file no_gdr_2.csv
---
+++ gdr_pingpong/viz_gdr_effect.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import argparse
+import os
+
+def format_data_size(size):
+    if size >= 1073741824:
+        return f'{size // 1073741824}G'
+    elif size >= 1048576:
+        return f'{size // 1048576}M'
+    elif size >= 1024:
+        return f'{size // 1024}K'
+    else:
+        return str(size)
+
+def plot_data(csv_files, output_file):
+    plt.figure(figsize=(18, 10))
+
+    for csv_file in csv_files:
+        # CSVファイルを読み込む
+        df = pd.read_csv(csv_file)
+
+        # 横軸のラベルをフォーマット
+        formatted_data_size = [format_data_size(size) for size in df['Data size']]
+
+        # CSVファイル名を拡張子を除いて取得
+        legend_name = os.path.splitext(os.path.basename(csv_file))[0]
+
+        # データをプロット
+        plt.plot(formatted_data_size, df['Bandwidth'], marker='o', linestyle='-', label=legend_name)
+
+    # フォントサイズを設定
+    plt.xlabel('Data size [Bytes]', fontsize=20)
+    plt.ylabel('Bandwidth [GB/s]', fontsize=20)
+    plt.xticks(ticks=range(len(formatted_data_size)), labels=formatted_data_size, rotation='vertical', fontsize=20, ha='center')
+    plt.yticks(fontsize=20)
+
+    # 凡例を表示（グラフのボックスの外、中央の上側、横並び）
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), fontsize=20, ncol=len(csv_files))
+
+    # 縦軸を0から始める
+    plt.ylim(0, max(df['Bandwidth']) * 1.1)
+
+    # グリッド線を消す
+    plt.grid(False)
+
+    # SVGファイルとして保存
+    plt.savefig(output_file, format='svg', bbox_inches='tight')
+
+    # グラフを表示
+    plt.show()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Plot data from CSV files.')
+    parser.add_argument('-i', '--input', nargs='+', required=True, help='Input CSV file(s)')
+    parser.add_argument('-o', '--output', required=True, help='Output SVG file')
+    args = parser.parse_args()
+
+    plot_data(args.input, args.output)
