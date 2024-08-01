adba273e8298b2486adb6a989d66347b7e300f9c
monotone-RK <kobaya4121@gmail.com>
2024-08-02 06:40:33 +0900

    GDRによるAllreduceの通信性能(バンド幅)を測定するスクリプトを追加

--- gdr_collective/Allreduce/run.sh
+++ gdr_collective/Allreduce/run.sh
@@ -2,12 +2,16 @@
 #------- qsub option -----------
 #PBS -A CCUSC
 #PBS -q gpu
-#PBS -l elapstim_req=00:15:00
+#PBS -l elapstim_req=01:00:00
 #PBS -T openmpi
-#PBS -b 2
+#PBS -b 16
 #PBS -v NQSV_MPI_VER=4.1.6/nvhpc23.1-cuda12.0
 
 #------- Program execution ----------
 cd $PBS_O_WORKDIR
 module load openmpi/"$NQSV_MPI_VER"
+mpirun ${NQSV_MPIOPTS} -report-bindings -bind-to none -np 1 -npernode 1 ./gdr_allreduce -max_elements 268435456 -num_iterations 100 -output_file 1_proc.csv
 mpirun ${NQSV_MPIOPTS} -report-bindings -bind-to none -np 2 -npernode 1 ./gdr_allreduce -max_elements 268435456 -num_iterations 100 -output_file 2_proc.csv
+mpirun ${NQSV_MPIOPTS} -report-bindings -bind-to none -np 4 -npernode 1 ./gdr_allreduce -max_elements 268435456 -num_iterations 100 -output_file 4_proc.csv
+mpirun ${NQSV_MPIOPTS} -report-bindings -bind-to none -np 8 -npernode 1 ./gdr_allreduce -max_elements 268435456 -num_iterations 100 -output_file 8_proc.csv
+mpirun ${NQSV_MPIOPTS} -report-bindings -bind-to none -np 16 -npernode 1 ./gdr_allreduce -max_elements 268435456 -num_iterations 100 -output_file 16_proc.csv
